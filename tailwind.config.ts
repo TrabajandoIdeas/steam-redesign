d1f5cf2b6c1fb72641ca48a11c971d6c6646a5a0
Diego Pousa <diegopousalp@gmail.com>
2023-11-12 20:32:29 -0300

    Configured colours in tailwind Added temp components page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,34 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    colors: {
+      transparent: "transparent",
+      text: {
+        main: "#F3F3F3",
+        dim: "#76808C"
       },
-    },
+      bg: {
+        main: "#0E141B",
+        highlight: "#1E2329",
+        hover: "#313843",
+        secondary: "#313843",
+        tertiary: "#212B45"
+      },
+      primary: "#66C0F4",
+      secondary: "#4B619B",
+      accent: {
+        green: "#A1CD44",
+        red: "#CD5444",
+        yellow: "#C1B15F"
+      }
+    }
   },
-  plugins: [],
-}
-export default config
+  plugins: []
+};
+export default config;
